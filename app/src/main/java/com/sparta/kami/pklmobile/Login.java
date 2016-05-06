2f515e57c48a18716350c283b91a5ff017fc0ccf
sukamto95 <7313026@student.unpar.ac.id>
2016-05-06 14:10:08 +0700

    Login dan registrasi web service

--- app/src/main/java/com/sparta/kami/pklmobile/Login.java
+++ app/src/main/java/com/sparta/kami/pklmobile/Login.java
@@ -1,6 +1,11 @@
 package com.sparta.kami.pklmobile;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    String NAMESPACE = "http://schemas.xmlsoap.org/wsdl";
+    String URL = "http://webtest.unpar.ac.id/pklws/pkl.php?wsdl";
+
+    String SOAP_ACTION = "login";
+    String METHOD_NAME = "login";
+    EditText editTextUserName;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,26 +32,76 @@ public class Login extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        editTextUserName = (EditText) findViewById(R.id.editTextUserName);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
         Button btnLogin = (Button) findViewById(R.id.buttonLogin);
         Button btnRegis = (Button) findViewById(R.id.buttonRegistrasi);
 
-        btnLogin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View arg0){
-                Intent i = new Intent(Login.this,Katalog.class);
-                startActivity(i);
-                finish();
-            }
-        });
+        btnLogin.setOnClickListener(this);
+        btnRegis.setOnClickListener(this);
+    }
 
-        btnRegis.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View arg0){
-                Intent i = new Intent(Login.this,Registrasi.class);
+    public void onClick(View v) {
+        Intent i;
+        Bundle b;
+
+        switch (v.getId()) {
+            case R.id.buttonRegistrasi:
+                i = new Intent(Login.this, Registrasi.class);
                 startActivity(i);
                 finish();
-            }
-        });
+                break;
+            case R.id.buttonLogin:
+                SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+
+                request.addProperty("username", editTextUserName.getText().toString());
+                request.addProperty("password", editTextPassword.getText().toString());
+
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                envelope.setOutputSoapObject(request);
+                HttpTransportSE androidHttpTRansport = new HttpTransportSE(URL);
+
+                try {
+                    androidHttpTRansport.call(SOAP_ACTION, envelope);
+                    SoapObject resultsRequestSOAP = (SoapObject) envelope.bodyIn;
+                    String record;
+                    record = resultsRequestSOAP.getProperty(0).toString();
+                    record = (record.substring(2, record.length() - 1) + ",");
+                    String response = "";
+                    String sid = "";
+                    int index = 1;
+                    int prevPointer = 0;
+                    for (int j = 0; j < record.length(); j++) {
+                        if (record.charAt(j) == ',') {
+                            if (index == 1) {
+                                response = record.substring(prevPointer, j - 1);
+                            }
+                            if (index == 2) {
+                                sid = record.substring(prevPointer, j - 1);
+                                System.out.println(sid);
+                            }
+                            prevPointer = j + 2;
+                            index++;
+                        }
+                    }
+                    if (response.equals("OK")) {
+                        b = new Bundle();
+                        b.putString("sid",sid);
+                        i = new Intent(Login.this, Katalog.class);
+                        i.putExtras(b);
+                        startActivity(i);
+                        finish();
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+
+        }
     }
 
 }
--- app/src/main/java/com/sparta/kami/pklmobile/Registrasi.java
+++ app/src/main/java/com/sparta/kami/pklmobile/Registrasi.java
@@ -6,11 +6,14 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -19,10 +22,44 @@ public class Registrasi extends AppCompatActivity implements View.OnClickListene
 
     private PickerDialogs pickerDialog;
 
+    String NAMESPACE = "http://schemas.xmlsoap.org/wsdl";
+    String URL = "http://webtest.unpar.ac.id/pklws/pkl.php?wsdl";
+
+    String SOAP_ACTION = "regpkl";
+    String METHOD_NAME = "regpkl";
+
+    //AlertDialog.Builder alertDialog;
+    EditText editTextNama;
+    EditText editTextAlamat;
+    EditText editTextNomorHP;
+    EditText editTextProduk;
+    EditText editTextEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
+
+        /*alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Registrasi");
+        alertDialog.setMessage("Registrasi Berhasil");
+        alertDialog.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // continue with delete
+            }
+        });
+        alertDialog.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+            }
+        });*/
+
+        editTextNama = (EditText) findViewById(R.id.editTextNama);
+        editTextAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        editTextNomorHP = (EditText) findViewById(R.id.editTextNomorHP);
+        editTextProduk = (EditText) findViewById(R.id.editTextProduk);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+
         Button btnBatal = (Button) findViewById(R.id.buttonBatal);
         Button btnSimpan = (Button) findViewById(R.id.buttonSimpan);
         btnBatal.setOnClickListener(this);
@@ -44,12 +81,24 @@ public class Registrasi extends AppCompatActivity implements View.OnClickListene
                 finish();
                 break;
             case R.id.buttonSimpan:
-                String email = ((TextView)findViewById(R.id.editTextEmail)).getText().toString();
-                String name = ((TextView)findViewById(R.id.editTextNama)).getText().toString();
-                String address = ((TextView)findViewById(R.id.editTextAlamat)).getText().toString();
-                String number = ((TextView)findViewById(R.id.editTextNomorHP)).getText().toString();
+                SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
                 String birthday = pickerDialog.getDate();
-                String product = ((TextView)findViewById(R.id.editTextProduk)).getText().toString();
+                request.addProperty("nama",editTextNama.getText().toString());
+                request.addProperty("alamat",editTextAlamat.getText().toString());
+                request.addProperty("nohp",editTextNomorHP.getText().toString());
+                request.addProperty("produkunggulan",editTextProduk.getText().toString());
+                request.addProperty("user",editTextEmail.getText().toString());
+                request.addProperty("tgllahir",birthday);
+
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                envelope.setOutputSoapObject(request);
+                HttpTransportSE androidHttpTRansport = new HttpTransportSE(URL);
+                try{
+                    androidHttpTRansport.call(SOAP_ACTION, envelope);
+                    SoapObject resultsRequestSOAP = (SoapObject)envelope.bodyIn;
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
 
                 i = new Intent(Registrasi.this,Login.class);
                 startActivity(i);
